c8b1b4818acb48615ca3403f850c1acfd974d3ba
weisj <31143295+weisJ@users.noreply.github.com>
2021-09-10 15:48:55 +0200

    Add basic references tests for line,rounded rect,circle and ellipse

--- jsvg/src/test/java/com/github/weisj/jsvg/ReferenceTest.java
+++ jsvg/src/test/java/com/github/weisj/jsvg/ReferenceTest.java
@@ -51,6 +51,16 @@ import com.github.weisj.jsvg.geometry.size.FloatSize;
 class ReferenceTest {
 
     @Test
+    void testLine() throws Exception {
+        compareImages("line1", wrapTag(100, 100, "<line x1='05' y1='05' x2='70' y2='51' stroke='blue'/>"));
+        compareImages("line2", wrapTag(100, 200, "<line x1='05' y1='05' x2='70' y2='51' stroke='green'/>"));
+        compareImages("line3", wrapTag(200, 200, "<line x1='05' y1='05' x2='70' y2='51' stroke='yellow'/>"));
+        compareImages("line4", wrapTag(100, 100, "<line x1='10' y1='15' x2='80' y2='51' stroke='red'/>"));
+        compareImages("line5", wrapTag(100, 200, "<line x1='10' y1='15' x2='80' y2='51' stroke='#000000'/>"));
+        compareImages("line6", wrapTag(200, 200, "<line x1='10' y1='15' x2='80' y2='51' stroke='black'/>"));
+    }
+
+    @Test
     void testRectangle() throws Exception {
         compareImages("rect1", wrapTag(100, 100, "<rect x='5' y='5' width='70' height='51' fill='blue'/>"));
         compareImages("rect2", wrapTag(100, 200, "<rect x='5' y='5' width='70' height='51' fill='green'/>"));
@@ -58,6 +68,42 @@ class ReferenceTest {
         compareImages("rect4", wrapTag(100, 100, "<rect x='10' y='15' width='80' height='51' fill='red'/>"));
         compareImages("rect5", wrapTag(100, 200, "<rect x='10' y='15' width='80' height='51' fill='#000000'/>"));
         compareImages("rect6", wrapTag(200, 200, "<rect x='10' y='15' width='80' height='51' fill='black'/>"));
+    }
+
+    @Test
+    void testRoundRectangle() throws Exception {
+        compareImages("roundRect1",
+                wrapTag(100, 100, "<rect x='5' y='5' width='70' height='51' rx='5' ry='0' fill='blue'/>"));
+        compareImages("roundRect2",
+                wrapTag(100, 200, "<rect x='5' y='5' width='70' height='51' rx='10' ry='5' fill='green'/>"));
+        compareImages("roundRect3",
+                wrapTag(200, 200, "<rect x='5' y='5' width='70' height='51' rx='20' ry='10' fill='yellow'/>"));
+        compareImages("roundRect4",
+                wrapTag(100, 100, "<rect x='10' y='15' width='80' height='51' rx='30' ry='0' fill='red'/>"));
+        compareImages("roundRect5",
+                wrapTag(100, 200, "<rect x='10' y='15' width='80' height='51' rx='15' ry='5' fill='#000000'/>"));
+        compareImages("roundRect6",
+                wrapTag(200, 200, "<rect x='10' y='15' width='80' height='51' rx='5' ry='10' fill='black'/>"));
+    }
+
+    @Test
+    void testCircle() throws Exception {
+        compareImages("circle1", wrapTag(100, 100, "<circle cx='40' cy='40' r='35' fill='blue'/>"));
+        compareImages("circle2", wrapTag(100, 200, "<circle cx='40' cy='40' r='35' fill='green'/>"));
+        compareImages("circle3", wrapTag(200, 200, "<circle cx='40' cy='40' r='35' fill='yellow'/>"));
+        compareImages("circle4", wrapTag(100, 100, "<circle cx='50' cy='55' r='40' fill='red'/>"));
+        compareImages("circle5", wrapTag(100, 200, "<circle cx='50' cy='55' r='40' fill='#000000'/>"));
+        compareImages("circle6", wrapTag(200, 200, "<circle cx='50' cy='55' r='40' fill='black'/>"));
+    }
+
+    @Test
+    void testEllipse() throws Exception {
+        compareImages("ellipse1", wrapTag(100, 100, "<ellipse cx='40' cy='40' rx='35' ry='5' fill='blue'/>"));
+        compareImages("ellipse2", wrapTag(100, 200, "<ellipse cx='40' cy='40' rx='35' ry='50' fill='green'/>"));
+        compareImages("ellipse3", wrapTag(200, 200, "<ellipse cx='40' cy='40' rx='35' ry='80' fill='yellow'/>"));
+        compareImages("ellipse4", wrapTag(100, 100, "<ellipse cx='50' cy='55' rx='40' ry='5' fill='red'/>"));
+        compareImages("ellipse5", wrapTag(100, 200, "<ellipse cx='50' cy='55' rx='40' ry='45' fill='#000000'/>"));
+        compareImages("ellipse6", wrapTag(200, 200, "<ellipse cx='50' cy='55' rx='40' ry='30' fill='black'/>"));
     }
 
     @Test
