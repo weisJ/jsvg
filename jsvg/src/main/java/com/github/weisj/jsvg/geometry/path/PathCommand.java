5dba49de1c3905d562409141a38fb80fa54ea290
Jannis Weis <31143295+weisJ@users.noreply.github.com>
2022-01-03 23:50:58 +0100

    Make PathCommand constructor protected

--- jsvg/src/main/java/com/github/weisj/jsvg/geometry/path/PathCommand.java
+++ jsvg/src/main/java/com/github/weisj/jsvg/geometry/path/PathCommand.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2021 Jannis Weis
+ * Copyright (c) 2021-2022 Jannis Weis
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction,
@@ -29,9 +29,9 @@ public abstract class PathCommand {
 
     public boolean isRelative = false;
 
-    public PathCommand() {}
+    protected PathCommand() {}
 
-    public PathCommand(boolean isRelative) {
+    protected PathCommand(boolean isRelative) {
         this.isRelative = isRelative;
     }
 
