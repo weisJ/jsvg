1c3f4c67bd9db99825e4fe1a23616f8db843d824
Jannis Weis <31143295+weisJ@users.noreply.github.com>
2022-01-03 23:46:34 +0100

    Remove unused method TextContainer::segments

--- jsvg/src/main/java/com/github/weisj/jsvg/nodes/text/TextContainer.java
+++ jsvg/src/main/java/com/github/weisj/jsvg/nodes/text/TextContainer.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2021 Jannis Weis
+ * Copyright (c) 2021-2022 Jannis Weis
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction,
@@ -52,10 +52,6 @@ abstract class TextContainer extends BaseRenderableContainerNode<TextSegment>
     protected AttributeFontSpec fontSpec;
     protected LengthAdjust lengthAdjust;
     protected @NotImplemented Length textLength;
-
-    protected @NotNull List<TextSegment> segments() {
-        return segments;
-    }
 
     @Override
     @MustBeInvokedByOverriders
