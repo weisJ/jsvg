3db700789c9e711f881e8f2b6636c2912a4efc52
Jannis Weis <31143295+weisJ@users.noreply.github.com>
2022-01-03 23:53:54 +0100

    Replace generic RuntimeException with more specific ones

--- jsvg/src/main/java/com/github/weisj/jsvg/parser/SVGLoader.java
+++ jsvg/src/main/java/com/github/weisj/jsvg/parser/SVGLoader.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2021 Jannis Weis
+ * Copyright (c) 2021-2022 Jannis Weis
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction,
@@ -102,7 +102,7 @@ public class SVGLoader {
             SAXParser saxParser = saxParserFactory.newSAXParser();
             xmlReader = saxParser.getXMLReader();
         } catch (ParserConfigurationException | SAXException e) {
-            throw new RuntimeException(e);
+            throw new IllegalStateException(e);
         }
     }
 
