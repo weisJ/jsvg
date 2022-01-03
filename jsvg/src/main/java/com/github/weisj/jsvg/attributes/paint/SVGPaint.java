5c2f4b158272fbabf0555ff6a86d3d90a5277aff
Jannis Weis <31143295+weisJ@users.noreply.github.com>
2022-01-03 23:34:00 +0100

    Throw exception when painting with sentinel paint
    Paints like currentColor, contextFill etc. aren't meant to be
    painted with directly. Instead, the RenderContext is responsible
    for resolving it to the correct color.

--- jsvg/src/main/java/com/github/weisj/jsvg/attributes/paint/SVGPaint.java
+++ jsvg/src/main/java/com/github/weisj/jsvg/attributes/paint/SVGPaint.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2021 Jannis Weis
+ * Copyright (c) 2021-2022 Jannis Weis
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction,
@@ -34,11 +34,11 @@ public interface SVGPaint {
     SVGPaint NONE = new SVGPaint() {
         @Override
         public void fillShape(@NotNull Graphics2D g, @NotNull MeasureContext measure, @NotNull Shape shape,
-                @Nullable Rectangle2D bounds) {}
+                @Nullable Rectangle2D bounds) { /* NONE shouldn't fill anything */ }
 
         @Override
         public void drawShape(@NotNull Graphics2D g, @NotNull MeasureContext measure, @NotNull Shape shape,
-                @Nullable Rectangle2D bounds) {}
+                @Nullable Rectangle2D bounds) { /* NONE shouldn't draw anything */ }
 
         @Override
         public String toString() {
@@ -48,11 +48,15 @@ public interface SVGPaint {
     SVGPaint CURRENT_COLOR = new SVGPaint() {
         @Override
         public void fillShape(@NotNull Graphics2D g, @NotNull MeasureContext measure, @NotNull Shape shape,
-                @Nullable Rectangle2D bounds) {}
+                @Nullable Rectangle2D bounds) {
+            throw new IllegalStateException("Sentinel color CURRENT_COLOR shouldn't be used for painting directly");
+        }
 
         @Override
         public void drawShape(@NotNull Graphics2D g, @NotNull MeasureContext measure, @NotNull Shape shape,
-                @Nullable Rectangle2D bounds) {}
+                @Nullable Rectangle2D bounds) {
+            throw new IllegalStateException("Sentinel color CURRENT_COLOR shouldn't be used for painting directly");
+        }
 
         @Override
         public String toString() {
@@ -62,11 +66,15 @@ public interface SVGPaint {
     SVGPaint CONTEXT_FILL = new SVGPaint() {
         @Override
         public void fillShape(@NotNull Graphics2D g, @NotNull MeasureContext measure, @NotNull Shape shape,
-                @Nullable Rectangle2D bounds) {}
+                @Nullable Rectangle2D bounds) {
+            throw new IllegalStateException("Sentinel color CONTEXT_FILL shouldn't be used for painting directly");
+        }
 
         @Override
         public void drawShape(@NotNull Graphics2D g, @NotNull MeasureContext measure, @NotNull Shape shape,
-                @Nullable Rectangle2D bounds) {}
+                @Nullable Rectangle2D bounds) {
+            throw new IllegalStateException("Sentinel color CONTEXT_FILL shouldn't be used for painting directly");
+        }
 
         @Override
         public String toString() {
@@ -76,11 +84,15 @@ public interface SVGPaint {
     SVGPaint CONTEXT_STROKE = new SVGPaint() {
         @Override
         public void fillShape(@NotNull Graphics2D g, @NotNull MeasureContext measure, @NotNull Shape shape,
-                @Nullable Rectangle2D bounds) {}
+                @Nullable Rectangle2D bounds) {
+            throw new IllegalStateException("Sentinel color CONTEXT_STROKE shouldn't be used for painting directly");
+        }
 
         @Override
         public void drawShape(@NotNull Graphics2D g, @NotNull MeasureContext measure, @NotNull Shape shape,
-                @Nullable Rectangle2D bounds) {}
+                @Nullable Rectangle2D bounds) {
+            throw new IllegalStateException("Sentinel color CONTEXT_STROKE shouldn't be used for painting directly");
+        }
 
         @Override
         public String toString() {
