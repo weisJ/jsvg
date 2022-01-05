2dca48a45a1b844c8a57c7b60a7c20fe616fdd0d
Jannis Weis <31143295+weisJ@users.noreply.github.com>
2022-01-05 16:11:46 +0100

    Make Text implement Transformable, HasClip and HasFilter
    Per the spec those are allowed on text elements.

--- jsvg/src/main/java/com/github/weisj/jsvg/nodes/text/Text.java
+++ jsvg/src/main/java/com/github/weisj/jsvg/nodes/text/Text.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2021 Jannis Weis
+ * Copyright (c) 2021-2022 Jannis Weis
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction,
@@ -21,12 +21,25 @@
  */
 package com.github.weisj.jsvg.nodes.text;
 
-import org.jetbrains.annotations.NotNull;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Point2D;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import com.github.weisj.jsvg.geometry.size.MeasureContext;
 import com.github.weisj.jsvg.nodes.Anchor;
+import com.github.weisj.jsvg.nodes.ClipPath;
+import com.github.weisj.jsvg.nodes.Mask;
+import com.github.weisj.jsvg.nodes.filter.Filter;
+import com.github.weisj.jsvg.nodes.prototype.HasClip;
+import com.github.weisj.jsvg.nodes.prototype.HasFilter;
+import com.github.weisj.jsvg.nodes.prototype.Transformable;
+import com.github.weisj.jsvg.nodes.prototype.impl.GeometryContextImpl;
 import com.github.weisj.jsvg.nodes.prototype.spec.Category;
 import com.github.weisj.jsvg.nodes.prototype.spec.ElementCategories;
 import com.github.weisj.jsvg.nodes.prototype.spec.PermittedContent;
+import com.github.weisj.jsvg.parser.AttributeNode;
 
 @ElementCategories({Category.Graphic, Category.TextContent})
 @PermittedContent(
@@ -34,11 +47,44 @@ import com.github.weisj.jsvg.nodes.prototype.spec.PermittedContent;
     anyOf = Anchor.class,
     charData = true
 )
-public final class Text extends LinearTextContainer {
+public final class Text extends LinearTextContainer implements Transformable, HasClip, HasFilter {
     public static final String TAG = "text";
+
+    private GeometryContextImpl geometryContext;
 
     @Override
     public @NotNull String tagName() {
         return TAG;
+    }
+
+    @Override
+    public void build(@NotNull AttributeNode attributeNode) {
+        super.build(attributeNode);
+        geometryContext = GeometryContextImpl.parse(attributeNode);
+    }
+
+    @Override
+    public @Nullable ClipPath clipPath() {
+        return geometryContext.clipPath();
+    }
+
+    @Override
+    public @Nullable Mask mask() {
+        return geometryContext.mask();
+    }
+
+    @Override
+    public @Nullable Filter filter() {
+        return geometryContext.filter();
+    }
+
+    @Override
+    public @Nullable AffineTransform transform() {
+        return geometryContext.transform();
+    }
+
+    @Override
+    public @NotNull Point2D transformOrigin(@NotNull MeasureContext context) {
+        return geometryContext.transformOrigin(context);
     }
 }
