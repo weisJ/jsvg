ca5dc66f0bcc1f9a40e085e34a225530209e6243
Jannis Weis <31143295+weisJ@users.noreply.github.com>
2023-02-14 23:14:36 +0100

    Pass transform for luminosity buffer explicitly instead of Graphics

--- jsvg/src/main/java/com/github/weisj/jsvg/util/ImageUtil.java
+++ jsvg/src/main/java/com/github/weisj/jsvg/util/ImageUtil.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2021 Jannis Weis
+ * Copyright (c) 2021-2023 Jannis Weis
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction,
@@ -43,8 +43,8 @@ public final class ImageUtil {
                 (int) Math.ceil(GeometryUtil.scaleYOfTransform(at) * height), BufferedImage.TYPE_INT_ARGB_PRE);
     }
 
-    public static @NotNull BufferedImage createLuminosityBuffer(@NotNull Graphics2D g, double width, double height) {
-        AffineTransform at = g.getTransform();
+    public static @NotNull BufferedImage createLuminosityBuffer(@NotNull AffineTransform at, double width,
+            double height) {
         return new BufferedImage(
                 (int) Math.ceil(GeometryUtil.scaleXOfTransform(at) * width),
                 (int) Math.ceil(GeometryUtil.scaleYOfTransform(at) * height), BufferedImage.TYPE_BYTE_GRAY);
