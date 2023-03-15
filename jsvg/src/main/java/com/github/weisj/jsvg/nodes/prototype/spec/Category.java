aa4f16b581bc37fdf5a289e09c7ef9df353d2ba0
Jannis Weis <31143295+weisJ@users.noreply.github.com>
2023-03-15 20:10:41 +0100

    Add None as node category

--- jsvg/src/main/java/com/github/weisj/jsvg/nodes/prototype/spec/Category.java
+++ jsvg/src/main/java/com/github/weisj/jsvg/nodes/prototype/spec/Category.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2021 Jannis Weis
+ * Copyright (c) 2021-2023 Jannis Weis
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction,
@@ -36,7 +36,8 @@ public enum Category {
     Shape,
     Structural,
     TextContent,
-    TextContentChild;
+    TextContentChild,
+    None;
 
     private final boolean effectivelyAllowed;
 
