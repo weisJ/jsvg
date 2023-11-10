dfafb13350c544312f74d55f59d46aee7737d864
Jannis Weis <31143295+weisJ@users.noreply.github.com>
2023-11-10 10:55:14 +0100

    Default to pure strokes if default value is specified for rendering hint

--- jsvg/src/test/java/com/github/weisj/jsvg/ReferenceTest.java
+++ jsvg/src/test/java/com/github/weisj/jsvg/ReferenceTest.java
@@ -30,7 +30,7 @@ import java.io.InputStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
- import java.util.Map;
+import java.util.Map;
 import java.util.Objects;
 import java.util.function.Supplier;
 
