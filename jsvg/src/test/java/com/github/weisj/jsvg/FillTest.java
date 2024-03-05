b81875e632598bd0aba4f97ced50970f21d10b15
Jannis Weis <31143295+weisJ@users.noreply.github.com>
2024-03-05 22:15:19 +0100

    Revert tolerance adjustment for FillTest

--- jsvg/src/test/java/com/github/weisj/jsvg/FillTest.java
+++ jsvg/src/test/java/com/github/weisj/jsvg/FillTest.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2022 Jannis Weis
+ * Copyright (c) 2022-2024 Jannis Weis
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction,
@@ -31,6 +31,6 @@ class FillTest {
 
     @Test
     void testFillAttributeOnSVGRoot() {
-        assertEquals(SUCCESS, compareImages("fillOnSVG.svg", 2));
+        assertEquals(SUCCESS, compareImages("fillOnSVG.svg"));
     }
 }
