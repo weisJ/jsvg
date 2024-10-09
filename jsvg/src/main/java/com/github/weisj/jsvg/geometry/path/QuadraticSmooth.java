9933fa368644130ead19bf9c21221f7c5780889a
Jannis Weis <31143295+weisJ@users.noreply.github.com>
2024-10-09 22:14:08 +0200

    Separate last kot for cubic and quadratic beziers.

--- jsvg/src/main/java/com/github/weisj/jsvg/geometry/path/QuadraticSmooth.java
+++ jsvg/src/main/java/com/github/weisj/jsvg/geometry/path/QuadraticSmooth.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2021-2023 Jannis Weis
+ * Copyright (c) 2021-2024 Jannis Weis
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction,
@@ -40,11 +40,10 @@ final class QuadraticSmooth extends PathCommand {
     @Override
     public void appendPath(@NotNull Path2D path, @NotNull BuildHistory hist) {
         Point2D.Float offset = offset(hist);
-        Point2D.Float knot = lastKnotReflection(hist);
+        Point2D.Float knot = lastKnotReflectionQuadratic(hist);
 
         path.quadTo(knot.x, knot.y, x + offset.x, y + offset.y);
-        hist.setLastPoint(path.getCurrentPoint());
-        hist.setLastKnot(knot);
+        hist.setLastQuadratic(path.getCurrentPoint(), knot);
     }
 
     @Override
