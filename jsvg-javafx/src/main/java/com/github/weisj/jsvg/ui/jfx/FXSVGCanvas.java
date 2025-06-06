ff77d2138ac22b5bf5a46754515dbe5ec09be2c4
SonarSonic <ollielansdell@hotmail.co.uk>
2025-06-06 12:11:06 +0100

    Add FXSVGCanvas + FXSVGCanvasSkin node implementation

---
+++ jsvg-javafx/src/main/java/com/github/weisj/jsvg/ui/jfx/FXSVGCanvas.java
@@ -0,0 +1,223 @@
+package com.github.weisj.jsvg.ui.jfx;
+
+import com.github.weisj.jsvg.SVGDocument;
+import com.github.weisj.jsvg.animation.AnimationPeriod;
+import com.github.weisj.jsvg.renderer.animation.Animation;
+import com.github.weisj.jsvg.ui.jfx.skin.FXSVGCanvasSkin;
+import com.github.weisj.jsvg.view.ViewBox;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.*;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Control;
+import javafx.scene.control.Skin;
+import javafx.scene.control.SkinBase;
+import javafx.util.Duration;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * A JavaFX node for displaying a {@link SVGDocument}}
+ */
+public class FXSVGCanvas extends Control {
+
+    public enum RenderBackend {
+        /**
+         * Renders directly to a {@link GraphicsContext} and benefits from some hardware acceleration.
+         * The majority of SVG's will render correctly, but some specific features e.g. Filters, Masks may not
+         */
+        JavaFX,
+        /**
+         * Renders using JSVG AWT implementation, which may be slower to update but may will display the SVG more accurately
+         */
+        AWT
+    }
+
+    private static final ViewBox DEFAULT_VIEW_BOX = null;
+    private static final RenderBackend DEFAULT_RENDER_BACKEND = RenderBackend.JavaFX;
+    private static final AnimationPeriod DEFAULT_ANIMATION = new AnimationPeriod(0, 0, false);
+
+    private final Timeline timeline = new Timeline();
+
+    public FXSVGCanvas() {
+        currentAnimation.addListener((observable, oldValue, newValue) -> {
+            setupAnimation(newValue);
+        });
+
+        currentAnimation.bind(Bindings.createObjectBinding(() -> {
+            if(!animated.get()) return DEFAULT_ANIMATION;
+            if(animation.get() != null) return animation.get();
+            SVGDocument document = getDocument();
+            return document == null ? DEFAULT_ANIMATION : document.animation();
+        }, document, animation, animated));
+
+        currentViewBox.bind(Bindings.createObjectBinding(() -> {
+            if(viewBox.get() != null) return viewBox.get();
+            SVGDocument document = getDocument();
+            return document == null ? DEFAULT_VIEW_BOX : document.viewBox();
+        }, viewBox, document));
+    }
+
+    public void setupAnimation(Animation animation){
+        timeline.getKeyFrames().clear();
+        timeline.getKeyFrames().add(new KeyFrame(Duration.millis(animation.startTime()), new KeyValue(animationElapsedTime, 0)));
+        timeline.getKeyFrames().add(new KeyFrame(Duration.millis(animation.endTime()), new KeyValue(animationElapsedTime, animation.endTime())));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.playFromStart();
+    }
+
+    ////////////////////////////////////////////////
+
+    /**
+     * Requests that the SVG Document be repainted
+     * In most cases this is not necessary, as the canvas will automatically be repainted when any properties (e.g. document, view-box, backend) are changed
+     */
+    public void repaint(){
+        SkinBase<?> skin = (SkinBase<?>) getSkin();
+        if(skin != null){
+            FXSVGCanvasSkin fxSkin = (FXSVGCanvasSkin) skin;
+            fxSkin.markDirty();
+        }
+    }
+
+    public void playAnimation(){
+        timeline.play();
+    }
+
+    public void pauseAnimation(){
+        timeline.play();
+    }
+
+    public void restartAnimation(){
+        timeline.playFromStart();
+    }
+
+    public void stopAnimation(){
+        timeline.stop();
+    }
+
+    ////////////////////////////////////////////////
+
+    private final ObjectProperty<@NotNull RenderBackend> renderBackend = new SimpleObjectProperty<>(DEFAULT_RENDER_BACKEND);
+
+    public @NotNull RenderBackend getRenderBackend() {
+        return renderBackend.get();
+    }
+
+    public @NotNull ObjectProperty<@NotNull RenderBackend> renderBackendProperty() {
+        return renderBackend;
+    }
+
+    public void setRenderBackend(@NotNull RenderBackend renderer) {
+        this.renderBackend.set(renderer);
+    }
+
+    ////////////////////////////////////////////////
+
+    private final ObjectProperty<@Nullable SVGDocument> document = new SimpleObjectProperty<>();
+
+    public @Nullable SVGDocument getDocument() {
+        return document.get();
+    }
+
+    public @NotNull ObjectProperty<@Nullable SVGDocument> documentProperty() {
+        return document;
+    }
+
+    public void setDocument(@Nullable SVGDocument document) {
+        this.document.set(document);
+    }
+
+    ////////////////////////////////////////////////
+
+    private final ObjectProperty<@Nullable ViewBox> currentViewBox = new SimpleObjectProperty<>();
+
+    public @Nullable ViewBox getCurrentViewBox() {
+        return currentViewBox.get();
+    }
+
+    public @Nullable ReadOnlyObjectProperty<@Nullable ViewBox> currentViewBoxProperty() {
+        return currentViewBox;
+    }
+
+    ////////////////////////////////////////////////
+
+    private final ObjectProperty<@Nullable ViewBox> viewBox = new SimpleObjectProperty<>(DEFAULT_VIEW_BOX);
+
+    public @Nullable ViewBox getViewBox() {
+        return viewBox.get();
+    }
+
+    public @NotNull ObjectProperty<@Nullable ViewBox> viewBoxProperty() {
+        return viewBox;
+    }
+
+    public void setViewBox(@Nullable ViewBox viewBox) {
+        this.viewBox.set(viewBox);
+    }
+
+    ////////////////////////////////////////////////
+
+    private final BooleanProperty animated = new SimpleBooleanProperty(true);
+
+    public boolean isAnimated() {
+        return animated.get();
+    }
+
+    public @NotNull BooleanProperty animatedProperty() {
+        return animated;
+    }
+
+    public void setAnimated(boolean animated) {
+        this.animated.set(animated);
+    }
+
+    ////////////////////////////////////////////////
+
+    private final LongProperty animationElapsedTime = new SimpleLongProperty();
+
+    public long getAnimationElapsedTime() {
+        return animationElapsedTime.get();
+    }
+
+    public @NotNull ReadOnlyLongProperty animationElapsedTimeProperty() {
+        return animationElapsedTime;
+    }
+
+    ////////////////////////////////////////////////
+
+    private final ObjectProperty<@NotNull Animation> currentAnimation = new SimpleObjectProperty<>(DEFAULT_ANIMATION);
+
+    public @NotNull Animation getCurrentAnimation() {
+        return currentAnimation.get();
+    }
+
+    public @NotNull ReadOnlyObjectProperty<@NotNull Animation> currentAnimationProperty() {
+        return currentAnimation;
+    }
+
+    ////////////////////////////////////////////////
+
+    private final ObjectProperty<@Nullable Animation> animation = new SimpleObjectProperty<>(null);
+
+    public @Nullable Animation getAnimation() {
+        return animation.get();
+    }
+
+    public @NotNull ObjectProperty<@Nullable Animation> animationProperty() {
+        return animation;
+    }
+
+    public void setAnimation(@Nullable Animation animation) {
+        this.animation.set(animation);
+    }
+
+    ////////////////////////////////////////////////
+
+    @Override
+    protected Skin<?> createDefaultSkin() {
+        return new FXSVGCanvasSkin(this);
+    }
+}
