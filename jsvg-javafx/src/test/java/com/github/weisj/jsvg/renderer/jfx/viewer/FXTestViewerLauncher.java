0edd77fd1ea6ce42d3a32195d3a38820eb1ceb81
SonarSonic <ollielansdell@hotmail.co.uk>
2025-06-06 12:13:31 +0100

    Add JavaFX Test Viewer application

---
+++ jsvg-javafx/src/test/java/com/github/weisj/jsvg/renderer/jfx/viewer/FXTestViewerLauncher.java
@@ -0,0 +1,9 @@
+package com.github.weisj.jsvg.renderer.jfx.viewer;
+
+public class FXTestViewerLauncher {
+
+    public static void main(String[] args) {
+        FXTestViewerApplication.launch(FXTestViewerApplication.class, args);
+    }
+
+}
