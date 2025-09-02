6e41a0769e796d55937233546f27235e35a8761c
Jannis Weis <31143295+weisJ@users.noreply.github.com>
2025-09-02 13:17:59 +0200

    Move Javafx renderer implementations into own package

--- jsvg-javafx/src/main/java/com/github/weisj/jsvg/ui/jfx/renderer/FXSVGRenderer.java
+++ jsvg-javafx/src/main/java/com/github/weisj/jsvg/ui/jfx/renderer/FXSVGRenderer.java
@@ -19,7 +19,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  */
-package com.github.weisj.jsvg.ui.jfx.skin;
+package com.github.weisj.jsvg.ui.jfx.renderer;
 
 import javafx.scene.Node;
 
